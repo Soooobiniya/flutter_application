26a0d2f3b22827cee57b1524d1440c2d5aef916b
Soooobiniya <rorngk22@gmail.com>
2023-12-11 16:52:55 +0900

    feat: BMI 계산기 기능 추가

---
+++ lib/Bmi/BmiMain.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/Bmi/BmiResult.dart';
+
+class BmiMain extends StatefulWidget {
+  @override
+  _BmiMainState createState() => _BmiMainState();
+}
+
+class _BmiMainState extends State<BmiMain> {
+  final _formKey = GlobalKey<FormState>(); // 폼의 상태를 얻기 위한 키
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('비만도 계산기')),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          // 폼
+          key: _formKey, // 키 할당
+          child: Column(
+            children: <Widget>[
+              // 키 입력 필드
+              TextFormField(
+                decoration: InputDecoration(
+                  // 외곽선이 있고 힌트로 '키'를 표시
+                  border: OutlineInputBorder(),
+                  hintText: '키',
+                ),
+                controller: _heightController,
+                keyboardType: TextInputType.number, // 숫자만 입력 가능
+                validator: (value) {
+                  if (value?.trim().isEmpty ?? true) {
+                    // 입력한 값의 앞뒤 공백을 제거한 것이 비었다면 에러 메시지 표시
+                    return '키를 입력하세요';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              // 몸무게 입력 필드
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '몸무게',
+                ),
+                controller: _weightController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value?.trim().isEmpty ?? true) {
+                    return '몸무게를 입력하세요';
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 폼에 입력된 값 검증
+                    if (_formKey.currentState != null &&
+                        _formKey.currentState!.validate()) {
+                      // 키와 몸무게 값이 검증되었다면 화면 이동
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => BmiResult(
+                                double.parse(_heightController.text.trim()),
+                                double.parse(_weightController.text.trim()))),
+                      );
+                    }
+                  },
+                  child: Text('결과'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/Bmi/BmiMain.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,59 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '폼 검증 데모',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('폼 검증 데모'),
-        ),
-        body: MyCustomForm(),
-      )
-
-    );
-  }
-}
-
-class MyCustomForm extends StatefulWidget {
-  @override
-  MyCustomFormState createState() => MyCustomFormState();
-}
-
-class MyCustomFormState extends State<MyCustomForm> {
-  // Form 위젯에 유니크한 키값을 부여하고 검증시 사용
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    // Form 위젯에 _formKey를 지정
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            validator: (value) {
-              if(value?.isEmpty ?? true) {
-                return '글자를 입력하세요.';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // 폼을 검증하여 통과하면 true, 실패하면 false 리턴
-                if (_formKey.currentState != null && _formKey.currentState!.validate()) {
-                  // 검증이 통과하면 스낵바 표시
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text('검증 완료')));
-                }
-              },
-              child: Text('검증'),
-            ),
-          )
-        ],
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: BmiMain(),
     );
   }
 }
